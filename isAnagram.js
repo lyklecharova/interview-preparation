4eb8eae68aab2f74c02f9e52a1b41d857d9fefaa
lyklecharova <lklecharova9@gmail.com>
2025-03-31 13:51:50 +0300

    Implemented isAnagram function
     - Checks if two strings are anagrams by sorting and comparing them.

---
+++ isAnagram.js
@@ -0,0 +1,11 @@
+function isAnagram(str1, str2) {
+    return str1
+        .split("")
+        .sort()
+        .join() ===
+        str2
+            .split("")
+            .sort()
+            .join();
+}
+console.log(isAnagram("listen", "silent"));
