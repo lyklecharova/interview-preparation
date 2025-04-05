b53b5d3cd08cf8fd4f0ae2fbe67ac7ba0de951f1
lyklecharova <lklecharova9@gmail.com>
2025-04-05 09:27:11 +0300

    Add iterative factorial function with example call

---
+++ factorial.js
@@ -0,0 +1,14 @@
+function factorial(n) {
+    if (n === 0) {
+        return 1;
+    }
+
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+
+    return result;
+}
+
+console.log(factorial(5)); // 120
